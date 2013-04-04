4a0a1f7b4687e39886fba288d5687724d5400470
Geoff Stokes <geoff@geoffstokes.net>
2013-04-04 12:46:53 +1100

    Optimised usage of "typeof"; implemented "isDefined" function for this. Un-minified object and function names in readable version. (Had to work around UglifyJS' behaviour of not renaming objects if "eval" is used by passing it in as something else). Net result is the bookmarklet is ~250 characters smaller when minified.

--- tech-audit.bookmarklet.js
+++ tech-audit.bookmarklet.js
@@ -1,90 +1,91 @@
-javascript:(function(w,u,b,n){
+javascript:(function(listDelimiter,unknownString,maskedEval){
 
-  var l = [],
-    s = [],
-    p = function(t){ l.push(t.join(" ")); },
-    q = function(v){ s.push(v); },
-    r = function(c){
-      var x = new XMLHttpRequest();
-        x.open('GET', w.location.toString(), true);
-        x.onreadystatechange = function(e) {
-          if (x.readyState == 4 && x.status == 200)
+  var clientSideList = [],
+    serverSideList = [],
+    isDefined = function(objectName){ return maskedEval("typeof " + objectName) != "undefined" },
+    clientSideListAdd = function(technology){ clientSideList.push(technology.join(" ")); },
+    serverSideListAdd = function(technology){ serverSideList.push(technology); },
+    requestPage = function(callback){
+      var currentXhr = new XMLHttpRequest();
+        currentXhr.open('GET', window.location.toString(), true);
+        currentXhr.onreadystatechange = function() {
+          if (currentXhr.readyState == 4 && currentXhr.status == 200)
           {
-            var h = x.getAllResponseHeaders().toString();
-            var i = h.split(/\r?\n/);
-            var k = {};
-            for (var j = 0; j < i.length; j++) {
-              var m = i[j].split(': ');
-              k[m[0]] = m[1];
+            var xhrHeaderString = currentXhr.getAllResponseHeaders().toString();
+            var xhrHeaderStrings = xhrHeaderString.split(/\r?\n/);
+            var xhrHeaderDictionary = {};
+            for (var i = 0; i < xhrHeaderStrings.length; i++) {
+              var currentHeaderSplit = xhrHeaderStrings[i].split(': ');
+              xhrHeaderDictionary[currentHeaderSplit[0]] = currentHeaderSplit[1];
             };
-            c(k);
+            callback(xhrHeaderDictionary);
           }
         };
-        x.send(null);
+        currentXhr.send(null);
     };
 
   // Plugins and frameworks
-  if (typeof WebForm_PostBackOptions != u) { p(["ASP.Net Web Forms"]); }
-  if (typeof Backbone != u)       { p(["Backbone.js", Backbone.VERSION]); }
-  if (typeof Cufon != u)          { p(["Cuf\u00f3n"]); }
-  if (typeof dojo != u)           { p(["Dojo", dojo.version.toString()]); }
-  if (typeof Foundation != u)     { p(["Zurb Foundation Core", Foundation.version]); }
-  if (typeof jQuery != u || (typeof $ != u && typeof $.jquery != u)) {
-    p(["jQuery", jQuery.fn.jquery || $.jquery]);
+  if (isDefined("WebForm_PostBackOptions")) { clientSideListAdd(["ASP.Net Web Forms"]); }
+  if (isDefined("Backbone"))       { clientSideListAdd(["Backbone.js", Backbone.VERSION]); }
+  if (isDefined("Cufon"))          { clientSideListAdd(["Cuf\u00f3n"]); }
+  if (isDefined("dojo"))           { clientSideListAdd(["Dojo", dojo.version.toString()]); }
+  if (isDefined("Foundation"))     { clientSideListAdd(["Zurb Foundation Core", Foundation.version]); }
+  if (isDefined("jQuery") || (isDefined("$") && isDefined("$.jquery"))) {
+    clientSideListAdd(["jQuery", jQuery.fn.jquery || $.jquery]);
     // jQuery Plug-Ins
-    if (typeof jQuery.ui != u ||
-      (typeof $.fn != u &&
-      typeof $.fn.ui != u)) { p(["jQuery UI ", jQuery.ui.version || $.ui.version]); }
-    if (typeof jQuery.fancybox != u ||
-      (typeof $.fn != u &&
-      typeof $.fn.fancybox != u)) { p(["jQuery Fancybox"]); }
+    if (isDefined("jQuery.ui") ||
+      (isDefined("$.fn") &&
+      isDefined("$.fn.ui"))) { clientSideListAdd(["jQuery UI ", jQuery.ui.version || $.ui.version]); }
+    if (isDefined("jQuery.fancybox") ||
+      (isDefined("$.fn") &&
+      isDefined("$.fn.fancybox"))) { clientSideListAdd(["jQuery Fancybox"]); }
   }
-  if (typeof Modernizr != u)      { p(["Modernizr", Modernizr._version]); }
-  if (typeof MooTools != u)       { p(["MooTools", MooTools.version]); }
-  if (typeof Prototype != u)      { p(["Prototype.js", Prototype.Version]); }
-  if (typeof requirejs != u ||
-      typeof require != u)        { p(["Require.js", requirejs.version || require.version]); }
-  if (typeof Scriptalicious != u) { p(["Scriptalicious", Scriptalicious.Version]); }
-  if (typeof Ext != u)            { p(["Sencha (ExtJS)", Ext.version]); }
-  if (typeof Sizzle != u)         { p(["Sizzle"]); }
-  if (typeof SC != u)             { p(["SproutCore", SC.VERSION]); }
-  if (typeof Spry != u)           { p(["Spry"]); }
-  if (typeof swfobject != u)      { p(["SWFObject"]); }
-  if (typeof Typekit != u)        { p(["Typekit"]); }
-  if (typeof Uize != u)           { p(["Uize"]); }
-  if (typeof _ != u &&
-      typeof _.VERSION != u)      { p(["Underscore.js", _.VERSION]); }
-  if (typeof YUI != u)            { p(["YUI", YUI.version]); }
-  if (typeof $ != u &&
-      typeof $.zepto != u)        { p(["Zepto"]); }
+  if (isDefined("Modernizr"))      { clientSideListAdd(["Modernizr", Modernizr._version]); }
+  if (isDefined("MooTools"))       { clientSideListAdd(["MooTools", MooTools.version]); }
+  if (isDefined("Prototype"))      { clientSideListAdd(["Prototype.js", Prototype.Version]); }
+  if (isDefined("requirejs") ||
+      isDefined("require"))        { clientSideListAdd(["Require.js", requirejs.version || require.version]); }
+  if (isDefined("Scriptalicious")) { clientSideListAdd(["Scriptalicious", Scriptalicious.Version]); }
+  if (isDefined("Ext"))            { clientSideListAdd(["Sencha (ExtJS)", Ext.version]); }
+  if (isDefined("Sizzle"))         { clientSideListAdd(["Sizzle"]); }
+  if (isDefined("SC"))             { clientSideListAdd(["SproutCore", SC.VERSION]); }
+  if (isDefined("Spry"))           { clientSideListAdd(["Spry"]); }
+  if (isDefined("swfobject"))      { clientSideListAdd(["SWFObject"]); }
+  if (isDefined("Typekit"))        { clientSideListAdd(["Typekit"]); }
+  if (isDefined("Uize"))           { clientSideListAdd(["Uize"]); }
+  if (isDefined("_") &&
+      isDefined("_.VERSION"))      { clientSideListAdd(["Underscore.js", _.VERSION]); }
+  if (isDefined("YUI"))            { clientSideListAdd(["YUI", YUI.version]); }
+  if (isDefined("$") &&
+      isDefined("$.zepto"))        { clientSideListAdd(["Zepto"]); }
 
   // Analytics and Performance Metrics
-  if (typeof udm_ != u)           { p(["ComScore"]); }
-  if (typeof _gauges != u)        { p(["Gauges"]); }
-  if (typeof _gaq != u)           { p(["Google Analytics"]); }
-  if (typeof Mint != u)           { p(["Mint"]); }
-  if (typeof Mixpanel != u)       { p(["Mixpanel"]); }
-  if (typeof NREUM != u)          { p(["New Relic"]); }
-  if (typeof Piwik != u)          { p(["Piwik"]); }
-  if (typeof __qc != u)           { p(["QuantCast"]); }
-  if (typeof ycsdone != u)        { p(["Yahoo Analytics"]); }
+  if (isDefined("udm_"))           { clientSideListAdd(["ComScore"]); }
+  if (isDefined("_gauges"))        { clientSideListAdd(["Gauges"]); }
+  if (isDefined("_gaq"))           { clientSideListAdd(["Google Analytics"]); }
+  if (isDefined("Mint"))           { clientSideListAdd(["Mint"]); }
+  if (isDefined("Mixpanel"))       { clientSideListAdd(["Mixpanel"]); }
+  if (isDefined("NREUM"))          { clientSideListAdd(["New Relic"]); }
+  if (isDefined("Piwik"))          { clientSideListAdd(["Piwik"]); }
+  if (isDefined("__qc"))           { clientSideListAdd(["QuantCast"]); }
+  if (isDefined("ycsdone"))        { clientSideListAdd(["Yahoo Analytics"]); }
 
   // Social Plugins
-  if (typeof addthis != u)        { p(["AddThis"]); }
-  if (typeof DISQUSWIDGETS != u)  { p(["Disqus"]); }
-  if (typeof FB != u)             { p(["Facebook Javascript SDK"]); }
-  if (typeof gapi != u)           { p(["Google+ API"]); }
-  if (typeof __twttrlr != u ||
-      typeof twttr != u)          { p(["Twitter Widgets"]); }
+  if (isDefined("addthis"))        { clientSideListAdd(["AddThis"]); }
+  if (isDefined("DISQUSWIDGETS"))  { clientSideListAdd(["Disqus"]); }
+  if (isDefined("FB"))             { clientSideListAdd(["Facebook Javascript SDK"]); }
+  if (isDefined("gapi"))           { clientSideListAdd(["Google+ API"]); }
+  if (isDefined("__twttrlr") ||
+      isDefined("twttr"))          { clientSideListAdd(["Twitter Widgets"]); }
 
-  if(confirm("Clientside Technology:\n" + b + (l.join(b) || n) + "\n\nDetect serverside technology?\n(Downloads the page again)"))
+  if(confirm("Clientside Technology:\n" + listDelimiter + (clientSideList.join(listDelimiter) || unknownString) + "\n\nDetect serverside technology?\n(Downloads the page again)"))
   {
-    r(function (o) {
-      if(typeof o['X-Powered-By'] != u) { q("Powered By: " + o['X-Powered-By']); }
-      if(typeof o.Server != u) { q("Server: " + o.Server); }
+    requestPage(function (response) {
+      if(typeof response['X-Powered-By'] != "undefined") { serverSideListAdd("Powered By: " + response['X-Powered-By']); }
+      if(typeof response.Server != "undefined") { serverSideListAdd("Server: " + response.Server); }
 
-      alert("Serverside Technology:\n" + b + (s.join(b) || n));
+      alert("Serverside Technology:\n" + listDelimiter + (serverSideList.join(listDelimiter) || unknownString));
     });
   }
 
-}(window,"undefined","\n• ","Unknown!"));
+}("\n• ","Unknown!",eval));
