b60af0ce77ccf7c52abaf2e6d901ade5a143e365
Jessica Stokes <hello@jessicastokes.net>
2017-03-02 17:21:11 +1100

    Improve `isDeeplyDefined` method

--- tech-audit.js
+++ tech-audit.js
@@ -4,17 +4,20 @@
   const clientSideList = [];
   const serverSideList = [];
 
-  const isDefined = (objectName) => {
-    const objectSplit = objectName.split(".");
+  const isDefined = (obj) => typeof obj !== 'undefined';
 
-    for (let pathIndex = 0; pathIndex < objectSplit.length; pathIndex++) {
-      if (eval("typeof " + objectSplit.slice(0, pathIndex + 1).join('.')) === "undefined") { // eslint-disable-line no-eval
-        return false;
-      }
-    }
-
-    return true;
-  };
+  const isDeeplyDefined = (objectPath) => (
+    isDefined(
+      objectPath.split('.').reduce(
+        (last, current) => (
+          isDefined(last)
+            ? last[current]
+            : undefined
+        ),
+        window
+      )
+    )
+  );
 
   const clientSideListAdd = (...technology) => clientSideList.push(technology.join(" "));
 
@@ -44,215 +47,215 @@
   };
 
   // Plugins and frameworks
-  if (isDefined("WebForm_PostBackOptions")) {
+  if (isDeeplyDefined("WebForm_PostBackOptions")) {
     clientSideListAdd("ASP.Net Web Forms");
   }
 
   /* global Backbone */
-  if (isDefined("Backbone")) {
+  if (isDeeplyDefined("Backbone")) {
     clientSideListAdd("Backbone.js", Backbone.VERSION);
   }
 
   /* global angular */
-  if (isDefined("angular")) {
+  if (isDeeplyDefined("angular")) {
     clientSideListAdd("Angular.js", angular.version.full, '(' + angular.version.codeName + ')');
   }
 
-  if (isDefined("Cufon")) {
+  if (isDeeplyDefined("Cufon")) {
     clientSideListAdd("Cuf\u00f3n");
   }
 
   /* global d3 */
-  if (isDefined("d3")) {
+  if (isDeeplyDefined("d3")) {
     clientSideListAdd("D3.js", d3.version);
   }
 
   /* global dojo */
-  if (isDefined("dojo"))                    {
+  if (isDeeplyDefined("dojo"))                    {
     clientSideListAdd("Dojo", dojo.version.toString());
   }
 
   /* global Foundation */
-  if (isDefined("Foundation")) {
+  if (isDeeplyDefined("Foundation")) {
     clientSideListAdd("Zurb Foundation Core", Foundation.version);
   }
 
   /* global jQuery, $ */
-  if (isDefined("jQuery.fn.jquery") || isDefined("$.fn.jquery")) {
+  if (isDeeplyDefined("jQuery.fn.jquery") || isDeeplyDefined("$.fn.jquery")) {
     clientSideListAdd("jQuery", jQuery.fn.jquery || $.fn.jquery);
   }
 
-  if (isDefined("jQuery.ui") || isDefined("$.ui")) {
+  if (isDeeplyDefined("jQuery.ui") || isDeeplyDefined("$.ui")) {
     clientSideListAdd("jQuery UI ", jQuery.ui.version || $.ui.version);
   }
 
-  if (isDefined("jQuery.fn.fancybox") || isDefined("$.fn.fancybox")) {
+  if (isDeeplyDefined("jQuery.fn.fancybox") || isDeeplyDefined("$.fn.fancybox")) {
     clientSideListAdd("jQuery Fancybox");
   }
 
   /* global Modernizr */
-  if (isDefined("Modernizr")) {
+  if (isDeeplyDefined("Modernizr")) {
     clientSideListAdd("Modernizr", Modernizr._version);
   }
 
   /* global MooTools */
-  if (isDefined("MooTools")) {
+  if (isDeeplyDefined("MooTools")) {
     clientSideListAdd("MooTools", MooTools.version);
   }
 
   /* global MOVIDEO */
-  if (isDefined("MOVIDEO")) {
+  if (isDeeplyDefined("MOVIDEO")) {
     clientSideListAdd("Movideo JavaScript SDK", MOVIDEO.version);
   }
 
   /* global Prototype */
-  if (isDefined("Prototype")) {
+  if (isDeeplyDefined("Prototype")) {
     clientSideListAdd("Prototype.js", Prototype.Version);
   }
 
   /* global requirejs, require */
-  if (isDefined("requirejs") || isDefined("require")) {
+  if (isDeeplyDefined("requirejs") || isDeeplyDefined("require")) {
     clientSideListAdd("Require.js", requirejs.version || require.version);
   }
 
   /* global Scriptalicious */
-  if (isDefined("Scriptalicious")) {
+  if (isDeeplyDefined("Scriptalicious")) {
     clientSideListAdd("Scriptalicious", Scriptalicious.Version);
   }
 
   /* global Ext */
-  if (isDefined("Ext")) {
+  if (isDeeplyDefined("Ext")) {
     clientSideListAdd("Sencha (ExtJS)", Ext.version);
   }
 
-  if (isDefined("Sizzle")) {
+  if (isDeeplyDefined("Sizzle")) {
     clientSideListAdd("Sizzle");
   }
 
   /* global SC */
-  if (isDefined("SC")) {
+  if (isDeeplyDefined("SC")) {
     clientSideListAdd("SproutCore", SC.VERSION);
   }
 
-  if (isDefined("Spry")) {
+  if (isDeeplyDefined("Spry")) {
     clientSideListAdd("Spry");
   }
 
-  if (isDefined("swfobject")) {
+  if (isDeeplyDefined("swfobject")) {
     clientSideListAdd("SWFObject");
   }
 
   /* global THREE */
-  if (isDefined("THREE")) {
+  if (isDeeplyDefined("THREE")) {
     clientSideListAdd("three.js", THREE.REVISION);
   }
 
-  if (isDefined("Typekit")) {
+  if (isDeeplyDefined("Typekit")) {
     clientSideListAdd("Typekit");
   }
 
-  if (isDefined("Uize")) {
+  if (isDeeplyDefined("Uize")) {
     clientSideListAdd("Uize");
   }
 
   /* global _ */
-  if (isDefined("_.VERSION")) {
+  if (isDeeplyDefined("_.VERSION")) {
     // Lo-Dash has a "support" object, Underscore does not.
-    if (isDefined("_.support")) {
+    if (isDeeplyDefined("_.support")) {
       clientSideListAdd("Lo-Dash", _.VERSION);
     } else {
       clientSideListAdd("Underscore.js", _.VERSION);
     }
   }
 
-  if (isDefined("YT")) {
+  if (isDeeplyDefined("YT")) {
     clientSideListAdd("YouTube iframe API");
   }
 
   /* global YUI */
-  if (isDefined("YUI")) {
+  if (isDeeplyDefined("YUI")) {
     clientSideListAdd("YUI", YUI.version);
   }
 
-  if (isDefined("$.zepto")) {
+  if (isDeeplyDefined("$.zepto")) {
     clientSideListAdd("Zepto");
   }
 
   // Analytics and Performance Metrics
-  if (isDefined("udm_")) {
+  if (isDeeplyDefined("udm_")) {
     clientSideListAdd("ComScore");
   }
 
-  if (isDefined("CE2")) {
+  if (isDeeplyDefined("CE2")) {
     clientSideListAdd("CrazyEgg");
   }
 
-  if (isDefined("_gauges")) {
+  if (isDeeplyDefined("_gauges")) {
     clientSideListAdd("Gauges");
   }
 
-  if (isDefined("_gaq")) {
+  if (isDeeplyDefined("_gaq")) {
     clientSideListAdd("Google Analytics (Old Syntax)");
   }
 
-  if (isDefined("ga")) {
+  if (isDeeplyDefined("ga")) {
     clientSideListAdd("Google Analytics");
   }
 
-  if (isDefined("Mint")) {
+  if (isDeeplyDefined("Mint")) {
     clientSideListAdd("Mint");
   }
 
-  if (isDefined("Mixpanel")) {
+  if (isDeeplyDefined("Mixpanel")) {
     clientSideListAdd("Mixpanel");
   }
 
-  if (isDefined("NREUM")) {
+  if (isDeeplyDefined("NREUM")) {
     clientSideListAdd("New Relic");
   }
 
-  if (isDefined("NolTracker") || isDefined("trac.pvar")) {
+  if (isDeeplyDefined("NolTracker") || isDeeplyDefined("trac.pvar")) {
     clientSideListAdd("NetRatings Site Census 6.x");
   }
 
-  if (isDefined("rsCi")) {
+  if (isDeeplyDefined("rsCi")) {
     clientSideListAdd("NetRatings Site Census 5.x");
   }
 
-  if (isDefined("opxLoad")) {
+  if (isDeeplyDefined("opxLoad")) {
     clientSideListAdd("Optimix");
   }
 
-  if (isDefined("Piwik")) {
+  if (isDeeplyDefined("Piwik")) {
     clientSideListAdd("Piwik");
   }
 
-  if (isDefined("__qc")) {
+  if (isDeeplyDefined("__qc")) {
     clientSideListAdd("QuantCast");
   }
 
-  if (isDefined("ycsdone")) {
+  if (isDeeplyDefined("ycsdone")) {
     clientSideListAdd("Yahoo Analytics");
   }
 
   // Social Plugins
-  if (isDefined("addthis")) {
+  if (isDeeplyDefined("addthis")) {
     clientSideListAdd("AddThis");
   }
 
-  if (isDefined("DISQUSWIDGETS")) {
+  if (isDeeplyDefined("DISQUSWIDGETS")) {
     clientSideListAdd("Disqus");
   }
 
-  if (isDefined("FB")) {
+  if (isDeeplyDefined("FB")) {
     clientSideListAdd("Facebook Javascript SDK");
   }
 
-  if (isDefined("gapi")) {
+  if (isDeeplyDefined("gapi")) {
     clientSideListAdd("Google+ API");
   }
 
-  if (isDefined("__twttrlr") || isDefined("twttr")) {
+  if (isDeeplyDefined("__twttrlr") || isDeeplyDefined("twttr")) {
     clientSideListAdd("Twitter Widgets");
   }
 
@@ -260,10 +263,10 @@
 
   if (confirm(`${clientSide}\n\nDetect serverside technology?\n(Downloads the page again)`)) {
     requestPage(function(response) {
-      if (typeof response['X-Powered-By'] != "undefined") {
+      if (isDefined(response['X-Powered-By'])) {
         serverSideListAdd("Powered By: " + response['X-Powered-By']);
       }
-      if (typeof response.Server != "undefined") {
+      if (isDefined(response.Server)) {
         serverSideListAdd("Server: " + response.Server);
       }
 
