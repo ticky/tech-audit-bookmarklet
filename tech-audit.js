c7c4b878a66b65e81091161b4b88e669b227afd2
Jessica Stokes <hello@jessicastokes.net>
2017-03-03 13:45:34 +1100

    More ES6 optimisations

--- tech-audit.js
+++ tech-audit.js
@@ -5,7 +5,6 @@
   const serverSideList = [];
 
   const isDefined = (obj) => typeof obj !== 'undefined';
-
   const isDeeplyDefined = (objectPath) => (
     isDefined(
       objectPath.split('.').reduce(
@@ -19,27 +18,24 @@
     )
   );
 
-  const clientSideListAdd = (...technology) => clientSideList.push(technology.join(" "));
-
-  const serverSideListAdd = (technology) => serverSideList.push(technology);
+  const listAdd = (list, ...technology) => list.push(technology.join(" "));
 
   const requestPage = (callback) => {
     const currentXhr = new XMLHttpRequest();
+    currentXhr.open('GET', window.location.href, true);
 
-    currentXhr.open('GET', window.location.toString(), true);
-
-    currentXhr.onreadystatechange = function() {
+    currentXhr.onreadystatechange = () => {
       if (currentXhr.readyState === 4 && currentXhr.status === 200) {
-        const xhrHeaderString = currentXhr.getAllResponseHeaders().toString();
-        const xhrHeaderStrings = xhrHeaderString.split(/\r?\n/);
-        const xhrHeaderDictionary = {};
+        const xhrHeaders = {};
 
-        for (let headerIndex = 0; headerIndex < xhrHeaderStrings.length; headerIndex++) {
-          const currentHeaderSplit = xhrHeaderStrings[headerIndex].split(': ');
-          xhrHeaderDictionary[currentHeaderSplit[0]] = currentHeaderSplit[1];
-        }
+        currentXhr.getAllResponseHeaders().toString()
+          .split(/\r?\n/)
+          .forEach((headerString) => {
+            const splitHeader = headerString.split(/\:\w*/);
+            xhrHeaders[splitHeader[0]] = splitHeader[1];
+          });
 
-        callback(xhrHeaderDictionary);
+        callback(xhrHeaders);
       }
     };
 
@@ -48,226 +44,231 @@
 
   // Plugins and frameworks
   if (isDeeplyDefined("WebForm_PostBackOptions")) {
-    clientSideListAdd("ASP.Net Web Forms");
+    listAdd(clientSideList, "ASP.Net Web Forms");
   }
 
   /* global Backbone */
   if (isDeeplyDefined("Backbone")) {
-    clientSideListAdd("Backbone.js", Backbone.VERSION);
+    listAdd(clientSideList, "Backbone.js", Backbone.VERSION);
   }
 
   /* global angular */
   if (isDeeplyDefined("angular")) {
-    clientSideListAdd("Angular.js", angular.version.full, '(' + angular.version.codeName + ')');
+    listAdd(clientSideList, "Angular.js", angular.version.full, '(' + angular.version.codeName + ')');
   }
 
   if (isDeeplyDefined("Cufon")) {
-    clientSideListAdd("Cuf\u00f3n");
+    listAdd(clientSideList, "Cuf\u00f3n");
   }
 
   /* global d3 */
   if (isDeeplyDefined("d3")) {
-    clientSideListAdd("D3.js", d3.version);
+    listAdd(clientSideList, "D3.js", d3.version);
   }
 
   /* global dojo */
   if (isDeeplyDefined("dojo"))                    {
-    clientSideListAdd("Dojo", dojo.version.toString());
+    listAdd(clientSideList, "Dojo", dojo.version.toString());
   }
 
   /* global Foundation */
   if (isDeeplyDefined("Foundation")) {
-    clientSideListAdd("Zurb Foundation Core", Foundation.version);
+    listAdd(clientSideList, "Zurb Foundation Core", Foundation.version);
   }
 
   /* global jQuery, $ */
   if (isDeeplyDefined("jQuery.fn.jquery") || isDeeplyDefined("$.fn.jquery")) {
-    clientSideListAdd("jQuery", jQuery.fn.jquery || $.fn.jquery);
+    listAdd(clientSideList, "jQuery", jQuery.fn.jquery || $.fn.jquery);
   }
 
   if (isDeeplyDefined("jQuery.ui") || isDeeplyDefined("$.ui")) {
-    clientSideListAdd("jQuery UI ", jQuery.ui.version || $.ui.version);
+    listAdd(clientSideList, "jQuery UI ", jQuery.ui.version || $.ui.version);
   }
 
   if (isDeeplyDefined("jQuery.fn.fancybox") || isDeeplyDefined("$.fn.fancybox")) {
-    clientSideListAdd("jQuery Fancybox");
+    listAdd(clientSideList, "jQuery Fancybox");
   }
 
   /* global Modernizr */
   if (isDeeplyDefined("Modernizr")) {
-    clientSideListAdd("Modernizr", Modernizr._version);
+    listAdd(clientSideList, "Modernizr", Modernizr._version);
   }
 
   /* global MooTools */
   if (isDeeplyDefined("MooTools")) {
-    clientSideListAdd("MooTools", MooTools.version);
+    listAdd(clientSideList, "MooTools", MooTools.version);
   }
 
   /* global MOVIDEO */
   if (isDeeplyDefined("MOVIDEO")) {
-    clientSideListAdd("Movideo JavaScript SDK", MOVIDEO.version);
+    listAdd(clientSideList, "Movideo JavaScript SDK", MOVIDEO.version);
   }
 
   /* global Prototype */
   if (isDeeplyDefined("Prototype")) {
-    clientSideListAdd("Prototype.js", Prototype.Version);
+    listAdd(clientSideList, "Prototype.js", Prototype.Version);
   }
 
-  /* global requirejs, require */
-  if (isDeeplyDefined("requirejs") || isDeeplyDefined("require")) {
-    clientSideListAdd("Require.js", requirejs.version || require.version);
+  /* global requirejs */
+  if (isDeeplyDefined("requirejs")) {
+    listAdd(clientSideList, "Require.js", requirejs.version);
+  }
+
+  if (isDeeplyDefined("require")) {
+    listAdd(clientSideList, "require");
   }
 
   /* global Scriptalicious */
   if (isDeeplyDefined("Scriptalicious")) {
-    clientSideListAdd("Scriptalicious", Scriptalicious.Version);
+    listAdd(clientSideList, "Scriptalicious", Scriptalicious.Version);
   }
 
   /* global Ext */
   if (isDeeplyDefined("Ext")) {
-    clientSideListAdd("Sencha (ExtJS)", Ext.version);
+    listAdd(clientSideList, "Sencha (ExtJS)", Ext.version);
   }
 
   if (isDeeplyDefined("Sizzle")) {
-    clientSideListAdd("Sizzle");
+    listAdd(clientSideList, "Sizzle");
   }
 
   /* global SC */
   if (isDeeplyDefined("SC")) {
-    clientSideListAdd("SproutCore", SC.VERSION);
+    listAdd(clientSideList, "SproutCore", SC.VERSION);
   }
 
   if (isDeeplyDefined("Spry")) {
-    clientSideListAdd("Spry");
+    listAdd(clientSideList, "Spry");
   }
 
   if (isDeeplyDefined("swfobject")) {
-    clientSideListAdd("SWFObject");
+    listAdd(clientSideList, "SWFObject");
   }
 
   /* global THREE */
   if (isDeeplyDefined("THREE")) {
-    clientSideListAdd("three.js", THREE.REVISION);
+    listAdd(clientSideList, "three.js", THREE.REVISION);
   }
 
   if (isDeeplyDefined("Typekit")) {
-    clientSideListAdd("Typekit");
+    listAdd(clientSideList, "Typekit");
   }
 
   if (isDeeplyDefined("Uize")) {
-    clientSideListAdd("Uize");
+    listAdd(clientSideList, "Uize");
   }
 
   /* global _ */
   if (isDeeplyDefined("_.VERSION")) {
     // Lo-Dash has a "support" object, Underscore does not.
     if (isDeeplyDefined("_.support")) {
-      clientSideListAdd("Lo-Dash", _.VERSION);
+      listAdd(clientSideList, "Lo-Dash", _.VERSION);
     } else {
-      clientSideListAdd("Underscore.js", _.VERSION);
+      listAdd(clientSideList, "Underscore.js", _.VERSION);
     }
   }
 
   if (isDeeplyDefined("YT")) {
-    clientSideListAdd("YouTube iframe API");
+    listAdd(clientSideList, "YouTube iframe API");
   }
 
   /* global YUI */
   if (isDeeplyDefined("YUI")) {
-    clientSideListAdd("YUI", YUI.version);
+    listAdd(clientSideList, "YUI", YUI.version);
   }
 
   if (isDeeplyDefined("$.zepto")) {
-    clientSideListAdd("Zepto");
+    listAdd(clientSideList, "Zepto");
   }
 
   // Analytics and Performance Metrics
   if (isDeeplyDefined("udm_")) {
-    clientSideListAdd("ComScore");
+    listAdd(clientSideList, "ComScore");
   }
 
   if (isDeeplyDefined("CE2")) {
-    clientSideListAdd("CrazyEgg");
+    listAdd(clientSideList, "CrazyEgg");
   }
 
   if (isDeeplyDefined("_gauges")) {
-    clientSideListAdd("Gauges");
+    listAdd(clientSideList, "Gauges");
   }
 
   if (isDeeplyDefined("_gaq")) {
-    clientSideListAdd("Google Analytics (Old Syntax)");
+    listAdd(clientSideList, "Google Analytics (Old Syntax)");
   }
 
   if (isDeeplyDefined("ga")) {
-    clientSideListAdd("Google Analytics");
+    listAdd(clientSideList, "Google Analytics");
   }
 
   if (isDeeplyDefined("Mint")) {
-    clientSideListAdd("Mint");
+    listAdd(clientSideList, "Mint");
   }
 
   if (isDeeplyDefined("Mixpanel")) {
-    clientSideListAdd("Mixpanel");
+    listAdd(clientSideList, "Mixpanel");
   }
 
   if (isDeeplyDefined("NREUM")) {
-    clientSideListAdd("New Relic");
+    listAdd(clientSideList, "New Relic");
   }
 
   if (isDeeplyDefined("NolTracker") || isDeeplyDefined("trac.pvar")) {
-    clientSideListAdd("NetRatings Site Census 6.x");
+    listAdd(clientSideList, "NetRatings Site Census 6.x");
   }
 
   if (isDeeplyDefined("rsCi")) {
-    clientSideListAdd("NetRatings Site Census 5.x");
+    listAdd(clientSideList, "NetRatings Site Census 5.x");
   }
 
   if (isDeeplyDefined("opxLoad")) {
-    clientSideListAdd("Optimix");
+    listAdd(clientSideList, "Optimix");
   }
 
   if (isDeeplyDefined("Piwik")) {
-    clientSideListAdd("Piwik");
+    listAdd(clientSideList, "Piwik");
   }
 
   if (isDeeplyDefined("__qc")) {
-    clientSideListAdd("QuantCast");
+    listAdd(clientSideList, "QuantCast");
   }
 
   if (isDeeplyDefined("ycsdone")) {
-    clientSideListAdd("Yahoo Analytics");
+    listAdd(clientSideList, "Yahoo Analytics");
   }
 
   // Social Plugins
   if (isDeeplyDefined("addthis")) {
-    clientSideListAdd("AddThis");
+    listAdd(clientSideList, "AddThis");
   }
 
   if (isDeeplyDefined("DISQUSWIDGETS")) {
-    clientSideListAdd("Disqus");
+    listAdd(clientSideList, "Disqus");
   }
 
   if (isDeeplyDefined("FB")) {
-    clientSideListAdd("Facebook Javascript SDK");
+    listAdd(clientSideList, "Facebook Javascript SDK");
   }
 
   if (isDeeplyDefined("gapi")) {
-    clientSideListAdd("Google+ API");
+    listAdd(clientSideList, "Google+ API");
   }
 
   if (isDeeplyDefined("__twttrlr") || isDeeplyDefined("twttr")) {
-    clientSideListAdd("Twitter Widgets");
+    listAdd(clientSideList, "Twitter Widgets");
   }
 
   const clientSide = `Clientside Technology:\n${listDelimiter + (clientSideList.join(listDelimiter) || unknownString)}`;
 
   if (confirm(`${clientSide}\n\nDetect serverside technology?\n(Downloads the page again)`)) {
-    requestPage(function(response) {
-      if (isDefined(response['X-Powered-By'])) {
-        serverSideListAdd("Powered By: " + response['X-Powered-By']);
+    requestPage((headers) => {
+      if (headers['X-Powered-By']) {
+        listAdd(serverSideList, "Powered By: " + headers['X-Powered-By']);
       }
-      if (isDefined(response.Server)) {
-        serverSideListAdd("Server: " + response.Server);
+
+      if (headers.Server) {
+        listAdd(serverSideList, "Server: " + headers.Server);
       }
 
       alert(`${clientSide}\n\nServerside Technology:\n${listDelimiter + (serverSideList.join(listDelimiter) || unknownString)}`);
